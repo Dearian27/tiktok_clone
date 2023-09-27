f423861d07450b81ab042a523ae35b0dd1f66c8f
dearian <dearian2706@gmail.com>
2023-09-27 22:31:32 +0300

    add main layouts

---
+++ app/layouts/includes/TopNav.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function TopNav() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <>
+      <div id="TopNav" className="fixed bg-white z-30 flex items-center w-full border-b h-[60px]">
+        <div className={`flex items-center justify-between mx-auto gap-6 w-full px-4 ${pathname == '/' ? 'max-w-[1150px]' : ''}`}>
+          <Link href={'/'}>
+            <img className="min-w[115px] w-[115px]" src="/images/tiktok_logo.png" alt="" />
+          </Link>
+        </div>
+      </div>
+    </>
+  )
+}
